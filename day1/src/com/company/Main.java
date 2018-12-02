27ee0d0db3649a42fbb013b7c3e4bd168fb3b480
Nichole Libby <nicholeklibby@gmail.com>
2018-12-01 18:03:31 -0700

    updates, with incorrect logic

--- day1/src/com/company/Main.java
+++ day1/src/com/company/Main.java
@@ -1,7 +1,9 @@
 package com.company;
 
+import java.io.File;
 import java.lang.Exception;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
@@ -21,9 +23,23 @@ public class Main {
         }
     }
 
-    static ArrayList<Integer> readValues(String args) {
-        ArrayList<Integer> intArray = null;
-        return intArray;
+    static ArrayList<Integer> readValues(String fileToRead) {
+        try {
+            Scanner scanner = new Scanner(new File(fileToRead));
+            ArrayList<Integer> intArray = new ArrayList<>();
+
+            while (scanner.hasNextLine()) {
+                intArray.add(scanner.nextLine().chars().sum());
+            }
+
+
+            return intArray;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     static int calculateTotal(ArrayList<Integer> toSum) {
